1815b1b2e79a543e753132e4ebc4a87da45825e4
Yong Wai <yongwaichong@gmail.com>
2025-02-26 10:56:06 +0800

    UPDATE

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const {dialogue} = require("./codyssey/dialogue")
 
+const allowedOrigins = [
+    "http://127.0.0.1:5500"
+]
+
 app.get('/', (req, res) => {
     res.json(dialogue);
     console.log(dialogue);
 });
+
+app.use(
+    cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
