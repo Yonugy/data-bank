1068d2fd69415a9db41dcdd87ce58018d9608cbf
Yong Wai <yongwaichong@gmail.com>
2025-03-18 21:33:38 +0800

    UPDATE

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ const app = express();
 const {dialogue} = require("./codyssey/dialogue")
 const {quest} = require("./codyssey/quest")
 
-
-const allowedOrigins = [
-    "http://127.0.0.1:5500"
-]
-
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500', // Replace with your frontend URL
+    origin: ["http://127.0.0.1:5500", "https://the-codyssey.vercel.app"], // Replace with your frontend URL
     methods: 'GET,POST', // Allowed HTTP methods
 };
 
