a5d87ba1d4301520f2005a806cc69858f07dbca5
Yong Wai <yongwaichong@gmail.com>
2025-03-22 18:25:00 +0800

    Second door (ownhouse) in town

--- codyssey/door.js
+++ codyssey/door.js
@@ -10,18 +10,27 @@ const door = {
       },
     },
 
+    {
+      to: "ownhouse",
+      position: {
+        x1: 117,
+        y1: 234,
+        x2: 157,
+        y2: 286,
+      },
+    },
   ],
 
   house1: [
     {
-        position: {
-          x1: 384,
-          y1: 832,
-          x2: 443,
-          y2: 895,
-        },
+      position: {
+        x1: 384,
+        y1: 832,
+        x2: 443,
+        y2: 895,
       },
-  ]
+    },
+  ],
 };
 
 module.exports = { door };
