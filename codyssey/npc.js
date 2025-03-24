aefa141fe26862113706d37a2b717c7b027f1cd3
Yong Wai <yongwaichong@gmail.com>
2025-03-24 17:16:11 +0800

    Npc new attribute (type, frameSize)

--- codyssey/npc.js
+++ codyssey/npc.js
@@ -1,23 +1,31 @@
 const npc = {
   bin: {
     name: "Bin",
+    type: "image",
     img: "trash_bin.png",
     scale: 0.1,
   },
   apu: {
     name: "APU",
+    type: "image",
     img: "apu_logo.png",
     scale: 0.5,
   },
   toilet: {
     name: "Toilet",
+    type: "image",
     img: "toilet.png",
     scale: 0.5,
   },
   cat: {
     name: "Cat",
+    type: "spritesheet",
     img: "cat-sheet.png",
     scale: 0.7,
+    frameSize: {
+        width: 247.5,
+        height: 247.5,
+    },
     animation: {
         cat_turn:{
             startFrame:0,
