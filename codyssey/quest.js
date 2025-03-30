36216dc9f3ffa2725d96768a58df0d24e2bc6d4b
Yong Wai <yongwaichong@gmail.com>
2025-03-30 12:55:58 +0800

    UPDATE

--- codyssey/quest.js
+++ codyssey/quest.js
@@ -1,113 +1,72 @@
 const quest = {
-  init:"quest1",
+  init: "quest1",
 
   quest1: {
-    startquest:"quest1intro",
+    startquest: "quest1intro",
     subquest: {
       quest1intro: {
         title: "a",
-        criteria:["end_quest1_intro"],
-        npc:{
-          bin:{
-            position: {x: 390, y: 400,}
-          },
-          apu:{
-            position: {x: 670, y: 440,}
-          },
-          toilet:{
-            position: {x: 980, y: 330,}
-          },
-          cat:{
-            position: {x: 330, y: 580,}
-          },
+        criteria: ["end_quest1_intro"],
+        npc: {
+          bin: { position: { x: 390, y: 400 } },
+          apu: { position: { x: 670, y: 440 } },
+          toilet: { position: { x: 980, y: 330 } },
+          cat: { position: { x: 330, y: 580 } },
         },
-        nextsubquest:"quest1start"
+        nextsubquest: "quest1start",
       },
 
       quest1start: {
         title: "b",
-        criteria:["done_shit_to_toilet"],
-        npc:{
-          bin:{
-            position: {x: 390, y: 400,}
-          },
-          apu:{
-            position: {x: 670, y: 440,}
-          },
-          toilet:{
-            position: {x: 980, y: 330,}
-          },
-          cat:{
-            position: {x: 330, y: 580,}
-          },
+        criteria: ["done_shit_to_toilet"],
+        npc: {
+          bin: { position: { x: 390, y: 400 } },
+          apu: { position: { x: 670, y: 440 } },
+          toilet: { position: { x: 980, y: 330 } },
+          cat: { position: { x: 330, y: 580 } },
         },
       },
     },
-
-    nextquest:"quest2"
+    nextquest: "quest2",
   },
 
   quest2: {
-    startquest:"quest2intro",
+    startquest: "quest2intro",
     subquest: {
       quest2intro: {
         title: "c",
-        criteria:["end_quest2_intro"],
-        npc:[
-          {
-            tag: bin,
-            position: {x: 390, y: 400,}
-          },
-          {
-            tag: apu,
-            position: {x: 670, y: 440,}
-          },
-          {
-            tag: toilet,
-            position: {x: 980, y: 330,}
-          },
-          {
-            tag: cat,
-            position: {x: 330, y: 580,}
-          },
-        ],
-        nextsubquest:"quest2start"
+        criteria: ["end_quest2_intro"],
+        npc: {
+          bin: { position: { x: 390, y: 400 } },
+          apu: { position: { x: 670, y: 440 } },
+          toilet: { position: { x: 980, y: 330 } },
+          cat: { position: { x: 330, y: 580 } },
+        },
+        nextsubquest: "quest2start",
       },
 
       quest2start: {
         title: "d",
-        criteria:["done_trash_to_cat"],
-        npc:[
-          {
-            tag: bin,
-            position: {x: 390, y: 400,}
-          },
-          {
-            tag: apu,
-            position: {x: 670, y: 440,}
-          },
-          {
-            tag: toilet,
-            position: {x: 980, y: 330,}
-          },
-          {
-            tag: cat,
-            position: {x: 330, y: 580,}
-          },
-        ],
+        criteria: ["done_trash_to_cat"],
+        npc: {
+          bin: { position: { x: 390, y: 400 } },
+          apu: { position: { x: 670, y: 440 } },
+          toilet: { position: { x: 980, y: 330 } },
+          cat: { position: { x: 330, y: 580 } },
+        },
       },
     },
-    nextquest:"quest3"
+    nextquest: "quest3",
   },
 
   quest3: {
     startquest: "quest3intro",
     subquest: {
-      quest3intro:{
+      quest3intro: {
         title: "e",
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 module.exports = { quest };
