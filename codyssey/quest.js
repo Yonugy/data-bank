c3377c056aca883715d28a9fea1a19aa87bf82f5
Yong Wai <yongwaichong@gmail.com>
2025-03-30 12:38:50 +0800

    Npc data in quest

--- codyssey/quest.js
+++ codyssey/quest.js
@@ -7,12 +7,40 @@ const quest = {
       quest1intro: {
         title: "a",
         criteria:["end_quest1_intro"],
+        npc:{
+          bin:{
+            position: {x: 390, y: 400,}
+          },
+          apu:{
+            position: {x: 670, y: 440,}
+          },
+          toilet:{
+            position: {x: 980, y: 330,}
+          },
+          cat:{
+            position: {x: 330, y: 580,}
+          },
+        },
         nextsubquest:"quest1start"
       },
 
       quest1start: {
         title: "b",
         criteria:["done_shit_to_toilet"],
+        npc:{
+          bin:{
+            position: {x: 390, y: 400,}
+          },
+          apu:{
+            position: {x: 670, y: 440,}
+          },
+          toilet:{
+            position: {x: 980, y: 330,}
+          },
+          cat:{
+            position: {x: 330, y: 580,}
+          },
+        },
       },
     },
 
@@ -25,12 +53,48 @@ const quest = {
       quest2intro: {
         title: "c",
         criteria:["end_quest2_intro"],
+        npc:[
+          {
+            tag: bin,
+            position: {x: 390, y: 400,}
+          },
+          {
+            tag: apu,
+            position: {x: 670, y: 440,}
+          },
+          {
+            tag: toilet,
+            position: {x: 980, y: 330,}
+          },
+          {
+            tag: cat,
+            position: {x: 330, y: 580,}
+          },
+        ],
         nextsubquest:"quest2start"
       },
 
       quest2start: {
         title: "d",
         criteria:["done_trash_to_cat"],
+        npc:[
+          {
+            tag: bin,
+            position: {x: 390, y: 400,}
+          },
+          {
+            tag: apu,
+            position: {x: 670, y: 440,}
+          },
+          {
+            tag: toilet,
+            position: {x: 980, y: 330,}
+          },
+          {
+            tag: cat,
+            position: {x: 330, y: 580,}
+          },
+        ],
       },
     },
     nextquest:"quest3"
